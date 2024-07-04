03327b3a5a1cf928151d4f507c2fc784536b72a0
Fouad Abdeen <abdeen.dev@gmail.com>
2024-07-04 07:08:24 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const baseApiUrl = "https://souk-el-kotob-api.onrender.com/";
+export const baseApiUrl = "https://souk-el-kotob-api.onrender.com";
 // "http://localhost:3030";
 export const apiHeaders = {
   "Content-Type": "application/json",
